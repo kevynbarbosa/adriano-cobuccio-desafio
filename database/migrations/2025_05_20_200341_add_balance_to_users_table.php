bd4b4c1918c8fff655ae706a2b567bb9720d6b41
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-05-21 17:22:54 -0300

    Ajuste document number para unique

--- database/migrations/2025_05_20_200341_add_balance_to_users_table.php
+++ database/migrations/2025_05_20_200341_add_balance_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->after('email', function (Blueprint $table) {
                 $table->string('account_number')->nullable();
-                $table->string('document_number')->nullable();
+                $table->string('document_number')->unique()->nullable();
                 $table->decimal('balance', 12, 2)->default(0);
             });
         });
