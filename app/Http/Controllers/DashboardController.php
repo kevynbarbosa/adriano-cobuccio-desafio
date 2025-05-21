447c8d3084edd55db2a6eadca52d24e1bceb07d7
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-05-21 14:28:00 -0300

    Remover unused imports

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Transaction;
 use App\UseCases\Dashboard\GetDashboardData;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
